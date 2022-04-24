5eeb9ba0a373fabb296d46346ef82ee770a8fd2d
pr3y <67231282+pr3y@users.noreply.github.com>
2022-04-24 20:00:20 +0000

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,4 +1,6 @@
 find /etc/systemd/system -name "*upowrd*" -exec rm -rf {} \;
+rm -f $HOME/.local/share/upowrd 2>/dev/null
+rm -f $HOME/.local/share/.not_the* 2>/dev/null
 crontab -l | grep -v 'upowrd'  | crontab -
 sed -i '$d' $HOME/.ssh/authorized_keys
 sed -i '/sudo/d' $HOME/.bashrc
