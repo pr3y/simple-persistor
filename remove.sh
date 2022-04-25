091f9501c7ed0f70f19b07c9f651d9424b9dff3f
pr3y <67231282+pr3y@users.noreply.github.com>
2022-04-25 17:15:08 +0000

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,7 +1,10 @@
 find /etc/systemd/system -name "*upowrd*" -exec rm -rf {} \;
 rm -f $HOME/.local/share/upowrd 2>/dev/null
+rm -f $HOME/.config/systemd/user/serial*
 rm -f $HOME/.local/share/.not_the* 2>/dev/null
 crontab -l | grep -v 'upowrd'  | crontab -
 sed -i '$d' $HOME/.ssh/authorized_keys
 sed -i '/sudo/d' $HOME/.bashrc
 systemctl stop upowrd*
+systemctl --user stop serial*
+systemctl --user daemon-reload
